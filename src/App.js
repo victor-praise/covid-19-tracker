386550e17441dbc79b8f0d3e47cd578bd64d638c
victor_praise <vpraise27@gmail.com>
2021-06-26 16:05:29 +0100

    added countries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState(['usa', 'india', 'test']);
+  useEffect(() => {
+    const getCountriesData = async () => {  
+      await fetch("https://disease.sh/v3/covid-19/countries").then(response=>response.json()).then((data) => {
+        const countries = data.map((country) => (
+          {
+            name:country.country,
+            value: country.countryInfo.iso2
+          }
+        ))
+        setCountries(countries);
+      })
+  
+    }
+    getCountriesData();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+     <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map(country=>
+                <MenuItem value={country.value}>
+                  {country.name}
+                </MenuItem>
+              )
+            }
+            {/* <MenuItem value="worldwide"> World Wide</MenuItem>
+            <MenuItem value="worldwide"> World</MenuItem>
+            <MenuItem value="worldwide"> Worldkk Wide</MenuItem>
+            <MenuItem value="worldwide"> World Wide</MenuItem> */}
+
+          </Select>
+        </FormControl>
+     </div>
+   
     </div>
   );
 }
